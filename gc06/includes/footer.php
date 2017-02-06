e96b71b8dfea91077662d0a98783e5e50b24e92e
chuntaomou <chuntaomou@users.noreply.github.com>
2017-02-06 18:39:04 +0800

    new header

--- gc06/includes/footer.php
+++ gc06/includes/footer.php
@@ -1,5 +1,5 @@
-<div id="footer">
-  <div class="row clearfix">
-    <img src="icons/logo.png" alt="Meet" height="100" width="85">
+<div class="row" style="background-color:#3b5998;">
+  <div class="col-sm-12" style="background-color:#3b5998;">
+    <h1><a href="homepage.php"><img src="icons/logo.png" class="img-rounded" alt="Meet" height="40" width="35"></a></h1>
   </div>
 </div>
