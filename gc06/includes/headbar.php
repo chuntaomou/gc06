0751c7f6feffba5de41eb0f0eae9fdaee8976d76
chuntaomou <chuntaomou@users.noreply.github.com>
2017-02-06 18:39:35 +0800

    new footer

--- gc06/includes/headbar.php
+++ gc06/includes/headbar.php
@@ -1,6 +1,8 @@
-<div id="header" role="banner">
-  <h1 id="logo"><a href="homepage.php"><img src="icons/logo.png" alt="Meet" height="40" width="35"></a></h1>
-  <div class="inner">
+<div class="row">
+  <div class="col-sm-8" style="background-color:#3b5998;">
+    <h1><a href="homepage.php"><img src="icons/logo.png" class="img-rounded" alt="Meet" height="40" width="35"></a></h1>
+  </div>
+  <div class="col-sm-4" style="background-color:#3b5998;">
       <ul id="quick_links" class="reset menu">
           <li><a href="#">Friend Request</a></li>
           <li><a href="#">My Account</a></li>
