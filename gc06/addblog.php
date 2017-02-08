a5b7974934dd89fc65adc0763c20d8e48459c6f3
chuntaomou <chuntaomou@users.noreply.github.com>
2017-02-09 02:41:41 +0800

    test newhomepage.php

--- gc06/addblog.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>GC06 - Add Blog</title>
-  <link rel="shortcut icon" href="icons/webicon.ico" type="image/x-icon">
-  <link href="css/gc06.css" rel="stylesheet" type="text/css">
-</head>
-</html>
