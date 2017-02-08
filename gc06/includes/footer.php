24e17493d7e7c8198230182e687abe0350e333fb
chuntaomou <chuntaomou@users.noreply.github.com>
2017-02-09 02:43:59 +0800

    test newhomepage.php

--- gc06/includes/footer.php
+++ gc06/includes/footer.php
@@ -1,5 +1,5 @@
 <div class="row" style="background-color:#3b5998;">
-  <div class="col-sm-12" style="background-color:#3b5998;">
-    <h1><a href="homepage.php"><img src="icons/logo.png" class="img-rounded" alt="Meet" height="40" width="35"></a></h1>
+  <div class="col-sm-12" style="background-color:#3b5998; height: 50px;">
+    <a href="homepage.php" style="maring-top: 15px;"><img src="../Icons/logo.png" class="img-rounded" alt="Meet" height="40" width="35"></a>
   </div>
 </div>
--- gc06/includes/headbar.php
+++ gc06/includes/headbar.php
@@ -1,11 +1,20 @@
+<!--
 <div class="row">
   <div class="col-sm-8" style="background-color:#3b5998;">
-    <h1><a href="homepage.php"><img src="icons/logo.png" class="img-rounded" alt="Meet" height="40" width="35"></a></h1>
+    <h1><a href="homepage.php"><img src="../Icons/logo.png" class="img-rounded" alt="Meet" height="40" width="35"></a></h1>
   </div>
   <div class="col-sm-4" style="background-color:#3b5998;">
       <ul id="quick_links" class="reset menu">
-          <li><a href="#">Friend Request</a></li>
-          <li><a href="#">My Account</a></li>
+          <li><a href="#" style="color:white;">Friend Request</a></li>
+          <li><a href="#" style="color:white;">My Account</a></li>
       </ul>
+  </div>
+</div>
+-->
+<div class="row">
+  <div class="col-sm-12" style="background-color:#3b5998;">
+    <a href="homepage.php"><img src="../Icons/logo.png" class="img-rounded" alt="Meet" height="40" width="35"></a>
+    <a href="#" style="color: white; float: right; padding-top: 10px;">My Account</a>
+    <a href="#" style="color: white; float: right; padding-top: 10px;">Friend Request | </a>
   </div>
 </div>
