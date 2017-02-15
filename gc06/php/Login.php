f8c9e029a85615a5960b84089029dbb806b92060
chuntaomou <chuntaomou@users.noreply.github.com>
2017-02-15 15:22:57 +0000

    change html to php

---
+++ gc06/php/Login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+
+    <title>Login Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+
+    <!-- include jquery -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <script type="text/javascript" src="../js/login.js"></script>
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="container">
+      <form class="form-signin" method="post">
+        <h2 class="form-signin-heading text-center">Meet</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="text" name="demail" class="form-control" placeholder="Username" id="email">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="password" class="form-control" placeholder="Password" id="password">
+        <!--<button class="btn btn-lg btn-primary btn-block" type="submit" id="Login">Sign in</button>-->
+        <input type="button" class="btn btn-lg btn-primary btn-block" name="login" id="login" value="Login">
+      </form>
+      <form class="form-signin" action="../php/registration.php">
+        <button class="btn btn-lg btn-info btn-block" type="submit">Register</button>
+      </form>
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+   <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
