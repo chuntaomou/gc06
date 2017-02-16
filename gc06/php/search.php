b25743987e807e4c563fa3cbf3101e7a0dfdbe46
chuntaomou <chuntaomou@users.noreply.github.com>
2017-02-16 17:30:43 +0000

    search php

---
+++ gc06/php/search.php
@@ -0,0 +1,24 @@
+<?php
+$connection = mysqli_connect('localhost','root','root','socialsite_db') or die('Error connecting to MySQL server.'. mysql_error());
+
+
+if(isset($_POST['searchVal'])){
+  $searchq=$_POST['searchVal'];
+  $searchq=preg_replace("#[^0-9a-z]#i","",$searchq);
+  $query="SELECT * FROM user_login WHERE user_name LIKE '%$searchq%'";
+  $result = mysqli_query($connection,$query);
+  //$row = mysqli_fetch_array($result);
+  $count=mysqli_num_rows($result);
+  if($count==0){
+    $output="there was no search result";
+  }else{
+    while($row=mysqli_fetch_array($result)){
+      $name=$row["user_name"];
+      $output .='<li><a href="">'.$name.'</a></li>';
+    }
+  }
+}else{
+  $output="no input";
+}
+echo $output;
+?>
