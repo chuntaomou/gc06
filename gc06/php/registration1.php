fa103e047091d8eec6915432b8b08a02e867fbc7
chuntaomou <chuntaomou@users.noreply.github.com>
2017-02-20 15:42:38 +0000

    latest version

--- gc06/php/registration1.php
+++ gc06/php/registration1.php
@@ -1,13 +1,11 @@
 <?php
-
+    session_start();
     include "../php/mysql_connect.php";
-
     ini_set('display_error', '1');
-
     ini_set('error_reporting', E_ALL);
 
 
-
+/*
     $email =  $_POST['email'];
 
     $password =  $_POST['password'];
@@ -25,5 +23,23 @@
 
 
     mysqli_close($connection);
+*/
+   $email=$_POST['email'];
+   $password=$_POST['password'];
+
+   $email=filter_var($email,FILTER_SANITIZE_EMAIL);
+   if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+     echo "Invalid Email.......";
+   }else{
+     $query = "INSERT INTO user_login (user_name, pass_word) VALUES ( '$email', SHA('$password') )";
+     $result = mysqli_query($connection, $query) or die('Error making select users query'.mysql_error());
+
+   }
+   if ($result) {
+     echo "1 record added";
+     $_SESSION['userid']=mysqli_insert_id($connection);
+     echo (mysqli_insert_id($connection));
+   }
+   mysqli_close($connection);
 
  ?>
