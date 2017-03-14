ba0d5e1e15b0dd11791ecf74cbad51deb9541a13
Lizhi Lv <tianli052124@gmail.com>
2017-03-14 14:41:58 +0000

    blog fixed

---
+++ gc06/html/Blogcontent.php
@@ -0,0 +1,103 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>GC06 - blogcontent</title>
+  <link rel="shortcut icon" href="../Icons/webicon.ico" type="image/x-icon">
+
+  <!-- Bootstrap core CSS -->
+  <link href="../css/bootstrap.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="../css/font-awesome.css" rel="stylesheet">
+  <link href="../css/style.css" rel="stylesheet">
+  <link href="../css/blog.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+
+
+</head>
+
+<body>
+
+  <?php require "../includes/headerforotherpages.php";
+  ?>
+
+
+  <div class="container">
+
+    <div class="blog-header">
+      <h1 class="blog-title">XX's Blog</h1>
+    </div>
+
+    <div class="row">
+
+      <div class="col-sm-8 blog-main">
+
+        <div class="blog-post">
+          <h2 class="blog-post-title">Blog1</h2>
+          <p>djhaskjdkashdkjhasdhaskdjkahciusakjnciuahi</p>
+
+
+        </div><!-- /.blog-post -->
+
+
+        <nav>
+          <ul class="pager">
+            <li><a href="#">Previous</a></li>
+            <li><a href="#">Next</a></li>
+          </ul>
+        </nav>
+
+      </div><!-- /.blog-main -->
+
+      <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+        <div class="sidebar-module sidebar-module-inset">
+          <h4>Privacy setting</h4>
+          <br>
+          <div class="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              select privacy level
+              <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+              <li><a href="#">Only me</a></li>
+              <li><a href="#">Friends</a></li>
+              <li><a href="#">Friend of Friend</a></li>
+            </ul>
+          </div>
+          <br>
+
+        </div>
+        <div class="sidebar-module">
+          <h4>Share to Socialmedia</h4>
+          <ol class="list-unstyled">
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Facebook</a></li>
+          </ol>
+        </div>
+      </div><!-- /.blog-sidebar -->
+
+    </div><!-- /.row -->
+
+  </div><!-- /.container -->
+
+
+
+  <!-- Bootstrap core JavaScript
+  ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+  <script src="../../dist/js/bootstrap.min.js"></script>
+  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
